ea21586bbc1d8f580775591bea9bd9e0c1890780
BaliseMtsweni <112752815+BaliseMtsweni@users.noreply.github.com>
2023-10-24 15:31:47 +0200

    Add files via upload

--- Campus_management_extension/models/extension.py
+++ Campus_management_extension/models/extension.py
@@ -7,3 +7,13 @@ class CampusExtension(models.Model):
     parent_name = fields.Char(string='Parent Name')
     parent_mobile = fields.Char(string='Parent Mobile')
 
+
+
+    def action_registration(self):
+        for rec in self:
+            if rec.state == 'draft':
+                rec.state ='in_progress'
+       
+
+    
+        
