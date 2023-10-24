619ab6bce6ba0712f7d70e319e730c3cece4b883
BaliseMtsweni <112752815+BaliseMtsweni@users.noreply.github.com>
2023-10-24 15:29:17 +0200

    Add files via upload

--- campus_management/models/students.py
+++ campus_management/models/students.py
@@ -4,7 +4,7 @@ from odoo.exceptions import ValidationError
 
 class CampusStudent(models.Model):
     _name = "campus.student"
-    _inherit = 'mail.thread'
+    _inherit = ['mail.thread', 'mail.activity.mixin']
     _description = "Student Records"
 
     name = fields.Char(string='Name', required=True, tracking=True)
@@ -18,6 +18,15 @@ class CampusStudent(models.Model):
     ref = fields.Char(string="Student Number", default=lambda self: _('New'))
     lecture_id = fields.Many2one('campus.lecture', string='Lecture')
     tag_ids = fields.Many2many('res.partner.category','campus_student_tag_rel','student_number','tag_id', string="Tags")
+    priority = fields.Selection([('0', 'Low'), ('1','Normarl'), ('2', 'High'), ('3', 'Very High')], string ="Priority")
+    state = fields.Selection([('draft', 'Draft'), ('in_progress','In Progress'), ('done', 'Done')],default='draft', string = "Status", required=True)
+    dob = fields.Date( string = 'Date Of Birth')
+    image = fields.Image(string ="Student Image")
+    color = fields.Char(string = "Color") 
+    active = fields.Boolean(string = "Active", default=True)
+    registration_id = fields.Many2one('campus.student', string ="Registration")
+    reason = fields.Text(string ="Reason")
+
 
     @api.model_create_multi
     def create(self, vals_lists):
@@ -45,5 +54,30 @@ class CampusStudent(models.Model):
             self.under_eighteen = True
         else:
             self.under_eighteen = False
+
+
+    def action_button(self):
+        print("Button Clicked")
+        return {
+            'effect':{
+                
+                    'fadeout' : 'slow',
+                    'message': 'Click Successfull',
+                    'type':'rainbow_man',
+
+            }
+
+        }
+
+    @api.depends('dob')
+    def _compute_age (self):
+        for rec in self:
+            today = date.today()
+            if rec.dob:
+                rec.age = today.year - rec.dob.year
+            else:
+                rec.age = 1
+
+        
 
 
